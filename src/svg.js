9ae77d6ec6d0c037901c37d267790b9383ad5d89
Marat Dyatko <i@marat.by>
2019-03-22 15:05:03 +0100

    v1.2.3

--- src/svg.js
+++ src/svg.js
@@ -1,20 +1,18 @@
 const fs = require("fs");
+const path = require("path");
 const d3 = require("d3");
 const D3Node = require("d3-node");
-const topojson = require("topojson");
 const color = require("color");
 const { argv } = require("./utils");
 const SVGO = require("svgo");
 
-const countryNames = new Map();
-const simplify = number => parseFloat(Number(number).toFixed(2));
-
-d3.tsvParse(
-  fs.readFileSync("./node_modules/world-atlas/world/50m.tsv").toString(),
-  data => countryNames.set(data.iso_n3, data.iso_a2)
+const stats = [];
+const countriesDataPath = path.resolve(
+  __dirname,
+  "../node_modules/visionscarto-world-atlas/world/50m_countries.geojson"
 );
 
-const stats = [];
+const simplify = number => parseFloat(Number(number).toFixed(2));
 const getColor = (name, stars, splits) => {
   if (!stars) return "#FFFFFF";
 
@@ -33,11 +31,16 @@ const getColor = (name, stars, splits) => {
     .hex();
 };
 
+const determineCountryCode = props => {
+  if (props.iso_a2 && props.iso_a2.match(/[A-Z]{2}/)) return props.iso_a2;
+  if (props.wb_a2 && props.wb_a2.match(/[A-Z]{2}/)) return props.wb_a2;
+  return props.sov_a3 ? props.sov_a3.substr(0, 2) : "";
+};
+
 module.exports.getSVG = ([countryPopularity]) => {
   const d3n = new D3Node();
   const svg = d3n.createSVG(640, 420);
-  const world = require("world-atlas/world/50m");
-  const countries = topojson.feature(world, world.objects.countries);
+  const countries = JSON.parse(fs.readFileSync(countriesDataPath).toString());
   const geoPath = d3.geoPath(
     d3.geoMercator().fitWidth(svg.attr("width"), countries)
   );
@@ -54,11 +57,11 @@ module.exports.getSVG = ([countryPopularity]) => {
       return d.id;
     })
     .style("fill", d => {
-      const name = countryNames.get(d.id);
-      const stars = countryPopularity.get(name);
-      countryPopularity.delete(name);
+      const code = determineCountryCode(d.properties);
+      const stars = countryPopularity.get(code);
+      countryPopularity.delete(code);
 
-      return getColor(name, stars, splits);
+      return getColor(code, stars, splits);
     })
     .style("stroke", "#afafaf")
     .style("stroke-width", "0.6px")
